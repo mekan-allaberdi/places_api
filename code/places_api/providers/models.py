43f0fcce8b46617bedb0d6bafaa5143437f5ebca
mekan.allaberdi <mekan.allaberdi@gmail.com>
2018-06-25 22:19:48 +0500

    Foursquare provider implementation.

--- code/places_api/providers/models.py
+++ code/places_api/providers/models.py
@@ -8,123 +8,181 @@ from places_api.util.constants import RADIUS
 
 
 class Provider(object):
-	"""
+    """
     Attributes:
         lat, lng (string) : latitude and longitude location value.
     """
-	def run(self, lat, lng):
-		self.location =  ','.join([lat, lng])
-		return self.proceed()
+    def run(self, lat, lng):
+        self.location =  ','.join([lat, lng])
+        return self.proceed()
 
 class GooglePlaces(Provider):
-	"""
+    """
     Attributes:
         url (string) : URL address of API.
         key (string) : Google Places API key
     """
-	def __init__(self):
-		self.url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-		self.key = 'AIzaSyAUTaT6h_6gfvmrM7hF_f3yP0sWtlZX6G4'
+    def __init__(self):
+        self.url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+        self.key = 'AIzaSyAUTaT6h_6gfvmrM7hF_f3yP0sWtlZX6G4'
 
-	def get_places_json(self):
-		"""
-	    Gets places json value from API.
+    def get_places_json(self):
+        """
+        Gets places json value from API.
 
-	    Returns:
-	        json
-	    """
-		params = {}
-		params['location'] = self.location
-		params['radius'] = RADIUS
-		params['key'] = self.key
-		data = get_response(self.url, params)
-		return data
+        Returns:
+            json
+        """
+        params = {}
+        params['location'] = self.location
+        params['radius'] = RADIUS
+        params['key'] = self.key
+        data = get_response(self.url, params)
+        return data
 
-	def get_place_id_list(self, places):
-		"""
-	    Parses and get place_id values from json.
+    def get_place_id_list(self, places):
+        """
+        Parses and get place_id values from json.
 
-	    Returns:
-	        place_id list
-	    """
-		place_id_list = []
-		for place in places:
-			if 'place_id' in place:
-				placeid = place['place_id']
-				place_id_list.append(placeid)
-		return place_id_list
+        Returns:
+            place_id list
+        """
+        place_id_list = []
+        for place in places:
+            if 'place_id' in place:
+                placeid = place['place_id']
+                place_id_list.append(placeid)
+        return place_id_list
 
-	def get_place_detail_list(self, place_id_list):
-		"""
-	    Gets place's details, parsing required fields.
+    def get_place_detail_list(self, place_id_list):
+        """
+        Gets place's details, parsing required fields.
 
-	    Returns:
-	        place details list
-	    """
-		num_of_places = len(place_id_list)
-		pool = ThreadPool(num_of_places)
-		for i in range(num_of_places):
-			google_place_details = GooglePlaceDetails(place_id_list[i])
-			pool.add_task(google_place_details.get_details)
-		
-		result = pool.get_result()
-		return result
+        Returns:
+            place details list
+        """
+        num_of_places = len(place_id_list)
+        pool = ThreadPool(num_of_places)
+        for i in range(num_of_places):
+            google_place_details = GooglePlaceDetails(place_id_list[i])
+            pool.add_task(google_place_details.get_details)
 
-	def proceed(self):
-		"""
-	    Root method of class.
+        result = pool.get_result()
+        return result
 
-	    Returns:
-	        list of place details
-	    """
-		places_json = self.get_places_json()
-		if not json_status_ok(places_json):
-			return []
+    def proceed(self):
+        """
+        Root method of class.
 
-		data = places_json['results']
-		place_id_list = self.get_place_id_list(data)
+        Returns:
+            list of place details
+        """
+        places_json = self.get_places_json()
+        if places_json['status'] != 'OK':
+            return []
 
-		get_place_detail_list = self.get_place_detail_list(place_id_list)
-		return get_place_detail_list
+        data = places_json['results']
+        place_id_list = self.get_place_id_list(data)
+
+        get_place_detail_list = self.get_place_detail_list(place_id_list)
+        return get_place_detail_list
 
 
 class GooglePlaceDetails():
-	"""
+    """
     Attributes:
-    	place_id : id of place from GooglePlaces API.
+        place_id : id of place from GooglePlaces API.
         url (string) : URL address of API.
         key (string) : Google Places API key
         attributes (dict) : Required attribute from json. 
     """
-	def __init__(self, place_id):
-		self.place_id = place_id
-		self.key = 'AIzaSyAUTaT6h_6gfvmrM7hF_f3yP0sWtlZX6G4'
-		self.url = 'https://maps.googleapis.com/maps/api/place/details/json'
-		self.attributes = {
-		'id': 'place_id',
-		'provider': 'scope',
-		'name': 'name',
-		'description': 'types',
-		'loaction': ['geometry', 'location'],
-		'address': 'formatted_address',
-		'uri': 'website'
-		}
+    def __init__(self, place_id):
+        self.place_id = place_id
+        self.key = 'AIzaSyAUTaT6h_6gfvmrM7hF_f3yP0sWtlZX6G4'
+        self.url = 'https://maps.googleapis.com/maps/api/place/details/json'
+        self.attributes = {
+        'id': 'place_id',
+        'provider': 'scope',
+        'name': 'name',
+        'description': 'types',
+        'loaction': ['geometry', 'location'],
+        'address': 'formatted_address',
+        'uri': 'website'
+        }
 
-	def get_details(self):
-		"""
-	    Gets detail of place from Api.
+    def get_details(self):
+        """
+        Gets detail of place from Api.
 
-	    Returns:
-	        place details dict
-	    """
-		params = {}
-		params['placeid'] = self.place_id
-		params['key'] = self.key
-		place_json = get_response(self.url, params)
-		if not json_status_ok(place_json):
-			return []
-		data = place_json['result']
-		place_details = parse_attributes(data, self.attributes)
-		return place_details
+        Returns:
+            place details dict
+        """
+        params = {}
+        params['placeid'] = self.place_id
+        params['key'] = self.key
+        place_json = get_response(self.url, params)
+        if place_json['status'] != 'OK':
+            return []
+        data = place_json['result']
+        place_details = parse_attributes(data, self.attributes)
+        return place_details
 
-	
+
+class Foursquare(Provider):
+    """
+    Attributes:
+        url (string) : URL address of API.
+        client_id (string) : Foursquare API client id
+        client_secret (string) : Foursquare API client secret
+        version (string) : Foursquare API date version. Format = YYYYMMDD
+    """
+
+    def __init__(self):
+        self.url = "https://api.foursquare.com/v2/venues/search"
+        self.oauth_token = 'KKFOT40HS4NHXZ3S5MZCJ3010VSEHL5YFNDMEV23OE2AB3H5'
+        self.version = '20180622'
+        self.attributes = {
+            'id': 'id',
+            'name': 'name',
+            'description': ['categories', 0, 'name'],
+            'location': ['location', ['lat', 'lng']],
+            'address': ['location', 'formattedAddress']
+        }
+
+    def get_places_json(self):
+        """
+        Gets places json value from API.
+
+        Returns:
+            json
+        """
+        params = {}
+        params['ll'] = self.location
+        params['oauth_token'] = self.oauth_token
+        params['v'] = self.version
+        data = get_response(self.url, params)
+        return data
+
+    def proceed(self):
+        """
+        Root method of class.
+
+        Returns:
+            list of place details
+        """
+        places_json = self.get_places_json()
+        code = places_json['meta']['code']
+        if code != 200:
+            return []
+
+        place_detail_list = []
+        data = places_json['response']['venues']
+        for place_data in data:
+            place_details = parse_attributes(place_data, self.attributes)
+            place_details['provider'] = 'foursquare'
+            place_detail_list.append(place_details)
+
+        return place_detail_list
+
+
+
--- code/places_api/util/helper.py
+++ code/places_api/util/helper.py
@@ -1,7 +1,7 @@
 import requests, json
 
 def get_response(url, params):
-	"""
+    """
     Gets response of url.
 
     Args:
@@ -10,12 +10,12 @@ def get_response(url, params):
     Returns:
         json result.
     """
-	resp = requests.get(url=url, params=params)
-	data = json.loads(resp.text)
-	return data
+    resp = requests.get(url=url, params=params)
+    data = json.loads(resp.text)
+    return data
 
-def json_status_ok(data_json):
-	"""
+def json_status_ok(data_json, status_attr):
+    """
     Check status of response json.
 
     Args:
@@ -23,11 +23,12 @@ def json_status_ok(data_json):
     Returns:
         status OK or NOT
     """
-	status = data_json['status']
-	return status == 'OK'
+    status = data_json[status_attr]
+    return status == 'OK'
+
 
 def get_tree_data(data, attributes):
-	"""
+    """
     Gets multiple inner tree data of json.
 
     Args:
@@ -36,15 +37,26 @@ def get_tree_data(data, attributes):
     Returns:
         element of json
     """
-	for attr in attributes:
-		if attr in data:
-			data = data[attr]
-		else:
-			return None
-	return data
+    for attr in attributes:
+        if type(attr) is int:
+            if attr < len(data):
+                data = data[attr]
+            else:
+                return None
+        elif type(attr) is list:
+            datas = {}
+            for key in attr:
+                if key in data:
+                    datas[key] = data[key]
+            return datas
+        elif attr in data:
+            data = data[attr]
+        else:
+            return None
+    return data
 
 def parse_attributes(data, attributes):
-	"""
+    """
     Parses json attributes and sends result.
 
     Args:
@@ -53,13 +65,12 @@ def parse_attributes(data, attributes):
     Returns:
         elements of json
     """
-	result = {}
-	for key, val in attributes.items():
-		attr_val = None
-		if type(val) is list:
-			attr_val = get_tree_data(data, val)
-		else:
-			attr_val = data.get(val, None)
-		if attr_val is not None:
-			result[key] = attr_val
-	return result
+    result = {}
+    for key, val in attributes.items():
+        if type(val) is list:
+            attr_val = get_tree_data(data, val)
+        else:
+            attr_val = data.get(val, None)
+        if attr_val is not None:
+            result[key] = attr_val
+    return result
